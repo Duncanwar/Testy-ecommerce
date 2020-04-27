fc03044fe361194ef76fa25e8247d41547236391
Duncan <semunda9@gmail.com>
2020-04-27 18:32:25 +0200

    able to link public folder with app.js and views

--- src/app.js
+++ src/app.js
@@ -1,12 +1,13 @@
 import express from 'express'
 import routes from './routes/routes'
-import ejs from 'ejs'
+
+import path from 'path'
 
 const app = express();
 
 app.use(express.json({limit:'50mb' }));
 app.use(express.urlencoded({ limit:'50mb' , extended:true }));
-app.use(express.static(`${__dirname}/public`));
+app.use(express.static(path.join(__dirname, '../public')));
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
